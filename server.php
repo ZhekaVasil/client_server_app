7dec440e003a5565f6280bd841a12a8c49198cd4
Zheka <zheka_1408@mail.ru>
2016-07-21 20:37:17 +0300

    all ready

--- server.php
+++ server.php
@@ -3,5 +3,5 @@ require_once 'config.php';
 $server = stream_socket_server("{$protocol}://{$server_ip}:{$port}", $ErrNo, $ErrStr);
 while($client = stream_socket_accept($server, -1))
 {
-    echo "К нам подключился новый клиент\n";
+    echo "New client\n";
 }
