505f7a5119ac38b9e4292fde701a50b1c1020b6a
Zheka <zheka_1408@mail.ru>
2016-07-21 21:15:28 +0300

    all ready

--- client.php
+++ client.php
@@ -1,5 +1,9 @@
 <?php
 require_once 'config.php';
+$name = $argv[1];
 
 $server = stream_socket_client("{$protocol}://{$server_ip}:{$port}", $ErrNo, $ErrStr );
-fwrite($server, "Hello server\n");
+fwrite($server, "how\n");
+fwrite($server, "{$name}\n");
+$response_server = stream_get_line($server, 1000, "\n");
+echo $response_server."\n";
--- server.php
+++ server.php
@@ -4,5 +4,15 @@ $server = stream_socket_server("{$protocol}://{$server_ip}:{$port}", $ErrNo, $Er
 while($client = stream_socket_accept($server, -1))
 {
     echo "New client detected\n";
+    $response_client = stream_get_line($client, 1000, "\n");
+    switch($response_client) {
+        case "hello":
+            fwrite($client, "Hello client\n");
+            break;
+        case "how":
+            $user = stream_get_line($client, 1000, "\n");
+            fwrite($client, "User {$user} is ok!\n");
+            break;
+    }
 
 }
